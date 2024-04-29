a64a8f9d3a115a1336e003eb77f0149af4b611aa
cz-sag <166489231+cz-sag@users.noreply.github.com>
2024-04-29 15:14:23 +0200

    Update user.nix

--- users/user.nix
+++ users/user.nix
@@ -3,9 +3,20 @@
   users.users.user = {
     isNormalUser  = true;
     home  = "/home/user";
-    description  = "Default user";
+    description  = "user";
     extraGroups  = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys  = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJe8i35RxB4wAbD+eL/uLHM2JdGcPfk78rCxE1zuyYyn user@nixos" ];
     initialHashedPassword = "$y$j9T$tvJEDhoWVGrQ1THRY3Y7.0$ArQpTFM1U02epH/ip/tjwe8EaAFIM7T2oE5Ok5sckQC";
+  };
+  home-manager = {
+    users.user = {
+      imports = [ ./user_home.nix ];
+      programs.git = {
+        enable = true;
+        userName = "user";
+        userEmail = "user@nixos";
+      };
+      home.stateVersion = "23.11";
+    };
   };
 }
