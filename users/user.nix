d4769995775dfe5bdb2cd5a504bec4dd64397b70
cz-sag <166489231+cz-sag@users.noreply.github.com>
2024-04-29 11:42:03 +0000

    Update user.nix

--- users/user.nix
+++ users/user.nix
@@ -6,6 +6,6 @@
     description  = "Default user";
     extraGroups  = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys  = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJe8i35RxB4wAbD+eL/uLHM2JdGcPfk78rCxE1zuyYyn user@nixos" ];
-    initialHashedPassword = "Password123";
+    initialHashedPassword = "$y$j9T$tvJEDhoWVGrQ1THRY3Y7.0$ArQpTFM1U02epH/ip/tjwe8EaAFIM7T2oE5Ok5sckQC";
   };
 }
