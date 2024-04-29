276aeed24828bf25cc7a1c8290bf02742c0db03c
cz-sag <you@example.com>
2024-04-29 11:23:46 +0000

    init password

--- users/user.nix
+++ users/user.nix
@@ -6,6 +6,6 @@
     description  = "Default user";
     extraGroups  = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys  = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJe8i35RxB4wAbD+eL/uLHM2JdGcPfk78rCxE1zuyYyn user@nixos" ];
-    users.users.your-user.initialPassword = “test”;
+    initialHashedPassword = "Password123";
   };
 }
