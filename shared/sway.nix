0808970c9bc43c8c9fb0e4af073444ee94e62532
user <user@nixos>
2025-02-12 08:58:37 +0100

    audio issue + virt and update

--- shared/sway.nix
+++ shared/sway.nix
@@ -10,6 +10,19 @@
     nautilus
   ];
 
+  
+  hardware.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
+  security.rtkit.enable = true; # Enable RealtimeKit for audio purposes
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # Uncomment the following line if you want to use JACK applications
+    # jack.enable = true;
+  };
+
   # Enable the gnome-keyring secrets vault. 
   # Will be exposed through DBus to programs willing to store secrets.
   services.gnome.gnome-keyring.enable = true;
--- shared/virt.nix
+++ shared/virt.nix
@@ -9,4 +9,9 @@
     pkgs.qemu_full  
   ];
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  programs.virt-manager.enable = true;
+  virtualisation.libvirtd.enable = true;
+  users.groups.libvirtd.members = ["user"];
+  virtualisation.spiceUSBRedirection.enable = true;
 }
